2bf023b77880671b3f2aabaf3053965def6c3b6c
Viviana <vyvy_escobar@hotmail.com>
2025-12-16 15:33:32 -0500

    Agrega función para sumar valores del formulario

--- js/app.js
+++ js/app.js
@@ -1,1 +1,11 @@
-console.log("aplicacion consolardora");
+function sumar() {
+  const forma = document.getElementById("forma");
+  let operandoA = forma["operandoA"];
+  let operandoB = forma["operandoB"];
+  let resultado = parseInt(operandoA.value) + parseInt(operandoB.value); //el parseInt me ayuda a sumar porque si no el me concatena
+  if (isNaN(resultado)) resultado = "La operacion no incluye numeros";
+  document.getElementById("resultado").innerHTML = `resultado: ${resultado}`;
+  console.log(`resultado: ${resultado}`);
+}
+
+//parseInt convierte un string a un numero
